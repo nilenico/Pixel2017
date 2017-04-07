104f46d91bb3698899fb07ac04bb3225a22f8e7b
F-OBeauchamp <pobeauchamp@hotmail.com>
2017-04-06 23:44:41 -0400

    Player Controller Update, Satellite

--- Pixel2017/Assets/Scripts/Controllers/PlayerController.cs
+++ Pixel2017/Assets/Scripts/Controllers/PlayerController.cs
@@ -4,19 +4,38 @@ using UnityEngine;
 
 public class PlayerController : Player {
 
+    public float movingSpeed;
+
+    void Start() {
+    }
+
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.W)) {
-            transform.position += Vector3.up;
+        checkInput();
+    }
+
+    private void checkInput() {
+
+        if (Input.GetKey(KeyCode.W)){
+            transform.position += Vector3.up * movingSpeed * Time.deltaTime;
         }
-        if (Input.GetKeyDown(KeyCode.A)) {
-            transform.position += Vector3.left;
+        if (Input.GetKey(KeyCode.A)){
+            transform.position += Vector3.left * movingSpeed * Time.deltaTime;
         }
-        if (Input.GetKeyDown(KeyCode.S)) {
-            transform.position += Vector3.down;
+        if (Input.GetKey(KeyCode.S)){
+            transform.position += Vector3.down * movingSpeed * Time.deltaTime;
         }
-        if (Input.GetKeyDown(KeyCode.D))  {
-            transform.position += Vector3.right;
+        if (Input.GetKey(KeyCode.D)){
+            transform.position += Vector3.right * movingSpeed * Time.deltaTime;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.tag.Equals("speed_boost"))
+        {
+            Debug.Log("Speed boosT");
+            movingSpeed *= 1.5f;
         }
     }
 }
