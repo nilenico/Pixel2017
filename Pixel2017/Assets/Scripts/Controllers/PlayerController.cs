2e343132fc0bc922f5f8cc3768237ffcf8ed5d2c
Nicolas Lepage <nicky-nico@hotmail.com>
2017-04-06 22:27:28 -0400

    Initial Commit 2D

---
+++ Pixel2017/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Player {
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.W)) {
+            transform.position += Vector3.up;
+        }
+        if (Input.GetKeyDown(KeyCode.A)) {
+            transform.position += Vector3.left;
+        }
+        if (Input.GetKeyDown(KeyCode.S)) {
+            transform.position += Vector3.down;
+        }
+        if (Input.GetKeyDown(KeyCode.D))  {
+            transform.position += Vector3.right;
+        }
+    }
+}
---
+++ Pixel2017/Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    List<Item> items;
+
+    //abstract PixelUpdate();
+
+    void Update() {
+        //PixelUpdate();
+    }
+}
