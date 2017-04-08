2220951b0187e15132e54236b937f0df36102c8f
F-OBeauchamp <pobeauchamp@hotmail.com>
2017-04-08 17:48:54 -0400

    Fix of the static ball, implemented the stun for the ball

--- Pixel2017/Assets/Scripts/Items/BlasterAmmo.cs
+++ Pixel2017/Assets/Scripts/Items/BlasterAmmo.cs
@@ -11,10 +11,17 @@ public class BlasterAmmo : MonoBehaviour {
     Vector3 direction;
     Vector3 origin;
     private float timeout;
+    private Vector3 lastPos;
+
+    private bool freezePlayer;
+    private GameObject playerHit;
+    private Vector3 freezePosition;
+    private float freezeTimeout = 100;
+    private float tempSpeed = 2.5f;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         origin = transform.position;
         timeout = 10;
         GetComponent<CircleCollider2D>().enabled = false;
@@ -26,33 +33,64 @@ public class BlasterAmmo : MonoBehaviour {
             velocity = Vector3.zero;
             velocity += trs.position - origin;
             transform.position += velocity * speed*Time.deltaTime;
-
     }
         if (timeout <= 0)
         {
             GetComponent<CircleCollider2D>().enabled = true;
+            checkIfMoving(transform.position);
         }
         else { timeout--; }
+        stun();
     }
 
     void OnTriggerEnter2D(Collider2D coll) {
         if (coll.gameObject.tag.Equals("Player"))
         {
             coll.GetComponent<Player>().gotShocked = true;
+            playerHit = coll.gameObject;
+            freezePosition = transform.position;
+            freezePlayer = true;
         }
         else if (coll.gameObject.tag.Equals("Target")) {
             Destroy(coll.gameObject);
             Destroy(this.gameObject);
         }
-        Debug.Log("coll "+coll.gameObject.name);
-        Debug.Log("trs " + trs.gameObject.name);
     }
 
     public void setTransform(Transform trs) {
         this.trs = trs;
     }
 
+    private void checkIfMoving(Vector3 currentPos) {
+        if (lastPos != null){
+            if (lastPos == currentPos){
+                Destroy(this.gameObject);
+            }
+            else { lastPos = currentPos; }
+        }
+        else { lastPos = currentPos; }
+    }
+
     public void setVelocity(Vector3 velocity){
         this.velocity = velocity;
+    }
+
+    private void stun()
+    {
+        if (freezePlayer)
+        {
+            if (freezeTimeout > 0)
+            {
+                playerHit.transform.position = freezePosition;
+                freezeTimeout--;
+            }
+            else
+            {
+                freezeTimeout = 100;
+                freezePosition = Vector3.zero;
+                playerHit.GetComponent<Player>().speed = tempSpeed;
+                playerHit = null;
+            }
+        }
     }
 }
