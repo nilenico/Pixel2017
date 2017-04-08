f8e9c27dfd341e1ed7f740cd6ddcbae1357b0fb4
Pier-Luc Fontaine <fontaine.pierluc@hotmail.com>
2017-04-07 22:49:12 -0400

    Started Wandering background

---
+++ Pixel2017/Assets/Scripts/Background/WanderingBackground.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingBackground : MonoBehaviour
+{
+
+    public GameObject[] stuff;
+
+
+    private float stuffWait;
+    private float stuffSpawnMostWait = 60.0f;    
+    private float stuffSpawnLeastWait = 15.0f;
+
+    private int startWait = 1;
+
+}
