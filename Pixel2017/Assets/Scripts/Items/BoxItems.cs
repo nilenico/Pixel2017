a65993f99a44767ec7537081be8180a764e11e95
Nicolas Lepage <nicky-nico@hotmail.com>
2017-04-08 15:00:35 -0400

    Merge

--- Pixel2017/Assets/Scripts/Items/BoxItems.cs
+++ Pixel2017/Assets/Scripts/Items/BoxItems.cs
@@ -6,16 +6,17 @@ public class BoxItems : MonoBehaviour {
 
     private GameObject choosenItem;
     private Object[] vPowerUps;
-
+    public float timeout;
     // Use this for initialization
     void Start () {
+        Destroy(this.gameObject,timeout);
         vPowerUps = Resources.LoadAll("Prefabs/BoostItems");
 	}
 
     void OnTriggerEnter2D(Collider2D coll) {
         if (coll.tag.Equals("Player")) {
+            Vector3 location = new Vector3(transform.position.x-0.2f, transform.position.y-0.2f, transform.position.z);
             choosenItem = Instantiate(vPowerUps[Random.Range(0, vPowerUps.Length)], transform.position, Quaternion.identity) as GameObject;
-            Debug.Log(choosenItem.name);
             Destroy(this.gameObject);
         }
     }
